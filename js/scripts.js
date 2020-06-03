7ff139412dadb1106af4d74254f55872d0d1b77b
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-03 10:40:59 -0700

    fix sytax errors in HTML, fix div placement, move button inside form
    Co-authored-by: Mook Dang <mookdang24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#formOne").submit(function(events) {
+    $("#formOne").submit(function(event) {
       const person1Input = $("input#person1").val();
       const person2Input = $("input#person2").val();
 
